0e8b42327d50f69acec3c2d8de39308257b49ded
jdar22169 <mmow313@gmail.com>
2016-07-11 13:53:51 -0700

    auth is working

--- model/user.js
+++ model/user.js
@@ -6,7 +6,6 @@ const secret = process.env.SECRET || 'changeme';
 const jwt = require('jsonwebtoken');
 
 const User = new mongoose.Schema({
-  email: {type: String, required: true},
   username: {type: String, required: true},
   password: {type: String, required: true}
 });
